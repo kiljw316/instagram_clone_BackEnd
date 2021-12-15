ddc4f56811259557dcf706d856637b4b27830675
kiljw316 <87928719+kiljw316@users.noreply.github.com>
2021-12-16 00:45:37 +0900

    Revert "Feature/post api register"

--- routers/user.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as userController from "../controllers/user.js";
-
-const router = express.Router();
-
-router.post("/register", userController.register);
-
-export default router;
