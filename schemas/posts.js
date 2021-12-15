1be7a82ac2b7cffd62e9613ee6945d50d9fb0d46
Seongjin Lee <babo318@gmail.com>
2021-12-16 02:44:46 +0900

    virtual 수정중

--- schemas/posts.js
+++ schemas/posts.js
@@ -1,22 +1,35 @@
 import mongoose from "mongoose";
 import likes from "./likes.js";
 
-const posts = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true,
+const posts = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    upload: {
+      type: Array,
+      required: true,
+    },
+    comments: {
+      type: Array,
+    },
   },
-  content: {
-    type: String,
-    required: true,
-  },
-  upload: {
-    type: Array,
-    required: true,
-  },
-  comments: {
-    type: Array,
-  },
+  { timestamps: true }
+);
+
+posts.virtual("likeCount").get(async function () {
+  const postId = this._id.toString();
+  const count = await likes.countDocuments({ postId });
+  return count;
+});
+
+posts.set({
+  virtuals: true,
 });
 
 export default mongoose.model("posts", posts);
